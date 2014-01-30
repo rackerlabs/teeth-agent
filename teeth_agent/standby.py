35d78287ab19454a0d935db6a1783689c4046f5a
Josh Gachnang <josh.gachnang@rackspace.com>
2014-01-30 15:31:50 -0800

    Adding authed download from Glance

--- teeth_agent/standby.py
+++ teeth_agent/standby.py
@@ -74,7 +74,11 @@ def _copy_configdrive_to_disk(configdrive_dir, device):
 
 
 def _request_url(image_info, url):
-    resp = requests.get(url, stream=True)
+    # Glance needs an auth token to download
+    headers = {}
+    if 'X-Keystone-Token' in image_info:
+        headers['X-Auth-Token'] = image_info['X-Keystone-Token']
+    resp = requests.get(url, stream=True, headers=headers)
     if resp.status_code != 200:
         raise errors.ImageDownloadError(image_info['id'])
     return resp
